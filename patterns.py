53a165c8200bfc1ad16d8363b5d4f62bb693db2d
bjarneo <bjarne.oeverli@gmail.com>
2014-07-02 19:34:14 +0200

    refactor

--- patterns.py
+++ patterns.py
@@ -6,18 +6,24 @@ from bs4 import BeautifulSoup
 
 class SubtlePatterns:
     url = 'http://subtlepatterns.com'
+    urls = []
 
     def __init__(self):
-        self.urls = []
-        self.get_image_urls()
+        self.run()
 
-    def get_image_urls(self):
-        opener = urllib2.build_opener()
-        
+    def run(self):
+        self.get_image_urls()
+        self.parse_image_urls()
+    
+    def get_image_urls(self):      
         # Set range 39 to how many pagination pages subtlepatters.com got. It's currently 39
+        # TODO: fetch last page url and get page number
         for counter in range(39, 0, -1):
             self.urls.append('%s/page/%s' % (self.url, counter))
-
+    
+    def parse_image_urls(self):
+        opener = urllib2.build_opener()        
+        
         try:
             for url in self.urls:
                 html = BeautifulSoup(opener.open(url).read())
@@ -28,7 +34,7 @@ class SubtlePatterns:
                                     
         except Exception, e:
             print e
-
+    
     def download_zips(self, zipurl):
         zipname = zipurl.split('/')[-1]
         print 'downloading %s ..' % zipname
